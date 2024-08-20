7733733c1dd46eb0cb6ff6473de2ba0e944f616e
Carlos Cares <carlos.cares@ceisufro.cl>
2024-08-20 00:33:03 -0400

    Update tu1_p04_producto_i.py
    Mejora de redacción tu1_p04

--- tu1_p04_producto_i.py
+++ tu1_p04_producto_i.py
@@ -1,12 +1,12 @@
-#Problema 4 tutorial 1
-#Para una secuencia de números
-#Programe el producto interior (elemento a elemento)
-#de los números en las posiciones pares respecto de las impares
-#(los elementos que faltan deben ser 0) 
+# Problema 4 tutorial 1
+# Para una secuencia de números
+# programe el producto interior (elemento a elemento)
+# de los números en las posiciones pares con aquellos en las posiciones impares
+# (los elementos que faltan deben ser 0) 
 #-------------------------------------------------------------
 #Ejemplo de Entrada
 # 1 2 3 6 2
-# debe consierarse (1 3 2) . (2 6 0) = 1x2 + 3x6 + 2x0 = 20
+# debe considerarse (1 3 2) x (2 6 0) = 1x2 + 3x6 + 2x0 = 20
 #Ejemplo de salida 20
 #-------------------------------------
 #Por ahora la lista entrega los elementos pares agregado un cero
